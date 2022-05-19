be788e4ba3cf67ccb232eccb68f3a7bb9b38319e
hsnkhvc <44241531+hsnkhvc@users.noreply.github.com>
2022-05-19 18:06:54 +0300

    sql-patikaEgitim3

---
+++ index.sql
@@ -0,0 +1,33 @@
+--1.soru
+
+Select *
+From country
+Where country LIKE 'A%a';
+
+
+--2.soru
+Select *
+From country
+Where country LIKE '_____%n';
+
+
+--3.soru
+select *
+from film
+where title ILIKE '%t%';
+
+
+--4.soru
+select title, length, rental_rate
+from film
+where title like 'C%' and length > 90 and rental_rate = 2.99;
+
+
+
+
+
+
+
+
+
+
